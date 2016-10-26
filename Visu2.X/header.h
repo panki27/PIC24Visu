2e5c193ef8709f5b63ad6ec103a1c2422e43e47e
panki27 <panki312@gmail.com>
2016-10-26 10:07:26 +0200

    Pankis Prebuilt stuff

---
+++ Visu2.X/header.h
@@ -0,0 +1,131 @@
+/* Microchip Technology Inc. and its subsidiaries.  You may use this software 
+ * and any derivatives exclusively with Microchip products. 
+ * 
+ * THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS".  NO WARRANTIES, WHETHER 
+ * EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED 
+ * WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A 
+ * PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION 
+ * WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION. 
+ *
+ * IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+ * INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
+ * WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS 
+ * BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE.  TO THE 
+ * FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS 
+ * IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF 
+ * ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *
+ * MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE 
+ * TERMS. 
+ */
+
+/* 
+ * File:   
+ * Author: 
+ * Comments:
+ * Revision history: 
+ */
+
+
+//LED CONFIG
+#define ON 1
+#define OFF 0
+#define INPUT 1
+#define OUTPUT 0
+
+#define LEDPIN1 TRISAbits.TRISA0
+#define LEDPIN2 TRISAbits.TRISA1
+#define LEDPIN3 TRISAbits.TRISA2
+#define LEDPIN4 TRISAbits.TRISA3
+#define LEDPIN5 TRISAbits.TRISA4
+#define LEDPIN6 TRISAbits.TRISA5
+#define LEDPIN7 TRISAbits.TRISA6
+#define LEDPIN8 TRISAbits.TRISA7
+
+#define LED1 LATAbits.LATA0
+#define LED2 LATAbits.LATA1
+#define LED3 LATAbits.LATA2
+#define LED4 LATAbits.LATA3
+#define LED5 LATAbits.LATA4
+#define LED6 LATAbits.LATA5
+#define LED7 LATAbits.LATA6
+#define LED8 LATAbits.LATA7
+
+
+// PIC24FJ128GA010 Configuration Bit Settings
+
+// 'C' source line config statements
+
+// CONFIG2
+#pragma config POSCMOD = HS             // Primary Oscillator Select (HS Oscillator mode selected)
+#pragma config OSCIOFNC = OFF           // Primary Oscillator Output Function (OSC2/CLKO/RC15 functions as CLKO (FOSC/2))
+#pragma config FCKSM = CSDCMD           // Clock Switching and Monitor (Clock switching and Fail-Safe Clock Monitor are disabled)
+#pragma config FNOSC = PRI              // Oscillator Select (Primary Oscillator (XT, HS, EC))
+#pragma config IESO = OFF               // Internal External Switch Over Mode (IESO mode (Two-Speed Start-up) disabled)
+
+// CONFIG1  //32768
+#pragma config WDTPS = PS32768         // Watchdog Timer Postscaler (1:32,768)
+#pragma config FWPSA = PR128            // WDT Prescaler (Prescaler ratio of 1:128)
+#pragma config WINDIS = ON              // Watchdog Timer Window (Standard Watchdog Timer enabled,(Windowed-mode is disabled))
+#pragma config FWDTEN = ON              // Watchdog Timer Enable (Watchdog Timer is enabled)
+#pragma config ICS = PGx2               // Comm Channel Select (Emulator/debugger uses EMUC2/EMUD2)
+#pragma config GWRP = OFF               // General Code Segment Write Protect (Writes to program memory are allowed)
+#pragma config GCP = OFF                // General Code Segment Code Protect (Code protection is disabled)
+#pragma config JTAGEN = OFF             // JTAG Port Enable (JTAG port is enabled)
+
+
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+
+// This is a guard condition so that contents of this file are not included
+// more than once.  
+#ifndef XC_HEADER_TEMPLATE_H
+#define	XC_HEADER_TEMPLATE_H
+
+#include <xc.h> // include processor files - each processor file is guarded.  
+
+// TODO Insert appropriate #include <>
+
+// TODO Insert C++ class definitions if appropriate
+
+// TODO Insert declarations
+
+// Comment a function and leverage automatic documentation with slash star star
+/**
+    <p><b>Function prototype:</b></p>
+  
+    <p><b>Summary:</b></p>
+
+    <p><b>Description:</b></p>
+
+    <p><b>Precondition:</b></p>
+
+    <p><b>Parameters:</b></p>
+
+    <p><b>Returns:</b></p>
+
+    <p><b>Example:</b></p>
+    <code>
+ 
+    </code>
+
+    <p><b>Remarks:</b></p>
+ */
+// TODO Insert declarations or function prototypes (right here) to leverage 
+// live documentation
+
+#ifdef	__cplusplus
+extern "C" {
+#endif /* __cplusplus */
+
+    // TODO If C++ is being used, regular C code needs function names to have C 
+    // linkage so the functions can be used by the c code. 
+
+#ifdef	__cplusplus
+}
+#endif /* __cplusplus */
+
+#endif	/* XC_HEADER_TEMPLATE_H */
+
---
+++ Visu2.X/main.c
@@ -0,0 +1,73 @@
+/*
+ * File:   main.c
+ * Author: Panki
+ *
+ * Created on 19. Oktober 2016, 13:41
+ */
+
+//Selfdefined
+#define FOSC 10000000
+#define FCY (FOSC/2)
+#include <libpic30.h>
+#include "lcd.h"
+//#include "lcd.c"
+
+#include "xc.h"
+#include "header.h"
+
+#include <p24fj128ga010.h>
+
+
+
+
+int main(void) { 
+      
+ LEDPIN1 = OUTPUT;
+ LEDPIN2 = OUTPUT;
+ LEDPIN3 = OUTPUT;
+ LEDPIN4 = OUTPUT;
+ LEDPIN5 = OUTPUT;
+ LEDPIN6 = OUTPUT;
+ LEDPIN7 = OUTPUT;
+ LEDPIN8 = OUTPUT;
+    
+ LCD_Initialize();
+ CG_Build();
+ LCD_PutChar(0x01);
+ LCD_PutChar(0x01);
+ LCD_PutChar(0x01);
+ //LCD_PutString("PANKI IS KING   DEUS VULT", 25);
+ 
+    while(1)
+  {
+        LED1 = ON;
+        __delay_ms(350);
+        LED2 = ON;
+        __delay_ms(350);
+        LED3 = ON;
+        __delay_ms(350);
+        LED4 = ON;
+        __delay_ms(350);
+        LED5 = ON;
+        __delay_ms(350);
+        LED6 = ON;
+        __delay_ms(350);
+        LED7 = ON;
+        __delay_ms(350);
+        LED8 = ON;
+        __delay_ms(350);
+        LED1 = OFF;
+        LED2 = OFF;
+        LED3 = OFF;
+        LED4 = OFF;
+        LED5 = OFF;
+        LED6 = OFF;
+        LED7 = OFF;
+        LED8 = OFF;
+        __delay_ms(350);
+    }
+  return 0;
+}
+
+
+
